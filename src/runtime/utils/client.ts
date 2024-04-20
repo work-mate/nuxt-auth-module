70733519dd477ef2ecc3dcf379ac9616b8782494
David Oyinbo <dev.davexoyinbo@gmail.com>
2024-04-20 15:22:27 +0100

    docstring

--- src/runtime/utils/client.ts
+++ src/runtime/utils/client.ts
@@ -1,17 +1,43 @@
 import { useRuntimeConfig } from "#app";
 import { AuthProvider } from "../providers/AuthProvider"
 
+
 let authClient: AuthProvider;
 
+/**
+ * Get the auth client singleton
+ * @returns {AuthProvider} - The auth client singleton
+ */
 export const getAuthClient = (): AuthProvider => {
-  if(authClient) return authClient;
+  /**
+   * If the auth client is already initialized, return it
+   */
+  if (authClient) {
+    return authClient;
+  }
 
+  /**
+   * Get the auth config from Nuxt runtime configuration
+   */
   const config = useRuntimeConfig().auth;
 
+  /**
+   * Create a new auth client with the config
+   */
   authClient = new AuthProvider({
+    /**
+     * The available auth providers from Nuxt runtime config
+     */
     providers: config.providers,
+    /**
+     * The default provider key to use from Nuxt runtime config,
+     * or fallback to "local" if not specified
+     */
     defaultProviderKey: config.defaultProvider || "local",
-  })
+  });
 
+  /**
+   * Return the new auth client
+   */
   return authClient;
-}//end
+};
