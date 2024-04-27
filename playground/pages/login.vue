f64b2990b40b263952f6bd3e9b253b345e40f6d5
David Oyinbo <dev.davexoyinbo@gmail.com>
2024-04-27 21:31:43 +0100

    .

---
+++ playground/pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Login Page:: Only guests can access this page
+    <pre>
+        {{ state }}
+      </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["auth-guest"],
+});
+
+const {
+  state
+} = useAuth();
+</script>
