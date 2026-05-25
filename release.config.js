947c4662e5a73c7006e68ea1d1dd2b6a24c752ed
David Oyinbo <dev.davexoyinbo@gmail.com>
2026-05-25 21:30:23 +0100

    chore: add conventional release to release config

--- release.config.js
+++ release.config.js
@@ -1,23 +1,30 @@
 export default {
   branches: [{ name: "v1", channel: "v1" }, "main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits", // 👈 Add this
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits", // 👈 Add this for consistent notes
+      },
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
       {
-        "changelogFile": "CHANGELOG.md"
-      }
+        changelogFile: "CHANGELOG.md",
+      },
     ],
     "@semantic-release/npm",
     [
       "@semantic-release/git",
       {
-        assets: [
-          "CHANGELOG.md",
-          "package.json",
-          "docs",
-        ],
+        assets: ["CHANGELOG.md", "package.json", "docs"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
